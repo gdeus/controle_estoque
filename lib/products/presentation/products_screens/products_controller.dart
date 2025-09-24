e3e68c198e35a85757818d3907ae6b165c8615a7
Gabriel Augusto de Deus <dedeus.gabriel@MacBook-Pro-de-Gabriel.local>
2025-09-23 21:47:32 -0300

    datasourcesss

--- lib/products/presentation/products_screens/products_controller.dart
+++ lib/products/presentation/products_screens/products_controller.dart
@@ -1,6 +1,6 @@
 import 'package:controle_estoque/products/domain/model/product.dart';
 import 'package:controle_estoque/products/domain/usecases/get_products.dart';
-import 'package:controle_estoque/products/external/get_products_mock_datasource.dart';
+import 'package:controle_estoque/products/external/datasources/get_products_mock_datasource.dart';
 
 class ProductsController {
   final GetProductsUsecase usecase = GetProductsUsecase(repository: GetProductsMockDatasource());
